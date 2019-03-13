19471e03a57ada69367175f30c5b2ac15fca4540
mattprice-28 <mattprice.28.mp@googlemail.com>
2019-03-13 10:00:12 +0000

    New Files

---
+++ projects/spring-installer/install.sh
@@ -0,0 +1,8 @@
+# create spring user
+`sudo useradd -m -s /bin/bash spring`
+
+# install java git maven
+`sudo apt install -y openjdk-8-jdk git`
+`sudo apt install -y maven`
+
+sudo cp spring.service /etc/systemd/system
